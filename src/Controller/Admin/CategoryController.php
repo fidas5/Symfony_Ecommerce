65cf5d89b7750b369059e09b89bc6c4919f14cdb
Fida Sassi <fidaasassi@gmail.com>
2024-11-10 14:52:18 +0100

    button added

--- src/Controller/Admin/CategoryController.php
+++ src/Controller/Admin/CategoryController.php
@@ -9,7 +9,7 @@ use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
-use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\String\Slugger\SluggerInterface;
 
 #[Route('/admin/category', name: 'admin_category_')]
@@ -18,11 +18,10 @@ class CategoryController extends AbstractController
     #[Route('/', name: 'index')]
     public function index(CategoryRepository $categoryRepository): Response
     {
-
-        $category = $categoryRepository->findBy([], ['categoryOrder' => 'ASC']);
+        $categories = $categoryRepository->findBy([], ['categoryOrder' => 'ASC']);
 
         return $this->render('admin/category/index.html.twig', [
-            'category' => $category
+            'categories' => $categories // Fix variable name here
         ]);
     }
 
@@ -32,7 +31,6 @@ class CategoryController extends AbstractController
         $this->denyAccessUnlessGranted('ROLE_ADMIN');
 
         $category = new Category();
-
         $categoryForm = $this->createForm(CategoryFormType::class, $category);
 
         $categoryForm->handleRequest($request);
@@ -51,5 +49,41 @@ class CategoryController extends AbstractController
         return $this->render('admin/category/add.html.twig', [
             'categoryForm' => $categoryForm,
         ]);
+    }
+
+    #[Route('/edit/{id}', name: 'edit')]
+    public function edit(Category $category, Request $request, EntityManagerInterface $em, SluggerInterface $slugger): Response
+    {
+        $this->denyAccessUnlessGranted('ROLE_ADMIN');
+
+        $categoryForm = $this->createForm(CategoryFormType::class, $category);
+        $categoryForm->handleRequest($request);
+
+        if ($categoryForm->isSubmitted() && $categoryForm->isValid()) {
+            $slug = $slugger->slug($category->getName());
+            $category->setSlug($slug);
+
+            $em->flush();
+
+            $this->addFlash('success', 'Category updated successfully!');
+            return $this->redirectToRoute('admin_category_index');
+        }
+
+        return $this->render('admin/category/edit.html.twig', [
+            'categoryForm' => $categoryForm,
+            'category' => $category,
+        ]);
+    }
+
+    #[Route('/delete/{id}', name: 'delete')]
+    public function delete(Category $category, EntityManagerInterface $em): Response
+    {
+        $this->denyAccessUnlessGranted('ROLE_ADMIN');
+
+        $em->remove($category);
+        $em->flush();
+
+        $this->addFlash('success', 'Category deleted successfully!');
+        return $this->redirectToRoute('admin_category_index');
     }
 }
